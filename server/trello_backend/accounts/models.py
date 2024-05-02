bc33ac23160d60defe3fd80fea2de2129a41132e
VitthalGund <vitthalgund30@gmail.com>
2024-05-02 23:07:50 +0530

    user model defined

--- server/trello_backend/accounts/models.py
+++ server/trello_backend/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    organization = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
